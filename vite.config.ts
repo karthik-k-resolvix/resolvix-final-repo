f3942c84d1068718a3642867adca15ed4ef8fbe6
mickey3522 <95731949+mickey3522@users.noreply.github.com>
2025-09-22 22:16:47 +0530

    sitemap.xml

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { createServer } from "./server";
+import { createServer } from "./server";  // or vue if you use Vue
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,7 +13,12 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: "dist/spa",
   },
-  plugins: [react(), expressPlugin()],
+  plugins: [react(), expressPlugin(), sitemap({
+      hostname: 'https://www.resolvix.tech',
+      // optional
+      // urls: ['/pricing', '/contact'],
+      // lastmod: true,
+    })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
